144f8450a5157e37e1359a239866bcd739215be1
Ariq Ramdhany <ariq.ramdhany@ittend.com>
2024-05-07 20:27:14 +0900

    tried animations

--- src/routes/Sentence.svelte
+++ src/routes/Sentence.svelte
@@ -1,28 +1,29 @@
 <script>
 	import { onMount } from 'svelte';
+	import { fly, fade } from 'svelte/transition';
 
-	let sentence = "";
-	const colors = ["", "green", "red"];
-	
+	let sentence = '';
+	const colors = ['', 'green', 'red'];
+
 	let counter = 0;
-	
+
 	let startTime;
-  let elapsedSeconds = 0;
+	let elapsedSeconds = 0;
 	let timer;
 
-	$: typed	= Array(sentence.length).fill(0); // 0 : untyped, 1 : correct, 2 : wrong
-	$: score = typed.filter(x => x === 1).length;
+	$: typed = Array(sentence.length).fill(0); // 0 : untyped, 1 : correct, 2 : wrong
+	$: score = typed.filter((x) => x === 1).length;
 	$: scorePercent = (score / sentence.length) * 100;
-	$: doneTyping=counter<sentence.length;
+	$: doneTyping = counter < sentence.length;
 
 	function onKeyDown(e) {
 		// const isAlphabetic = /^[a-zA-Z,. ?]$/.test(e.key);
-		const disallowedKeys = ["Control", "Alt", "Meta", "Escape", "Tab", "Shift"];
+		const disallowedKeys = ['Control', 'Alt', 'Meta', 'Escape', 'Tab', 'Shift'];
 		if (doneTyping) {
 			if (counter == 0) {
 				startTimer();
 			}
-			if(!disallowedKeys.includes(e.key)) {
+			if (!disallowedKeys.includes(e.key)) {
 				if (e.key === sentence[counter]) {
 					typed[counter] = 1;
 					console.log(`${e.key} ${typed[counter]}`);
@@ -35,50 +36,61 @@
 					console.log(`${e.key} ${typed[counter]}`);
 					counter++;
 				}
-		} 
-		if(!doneTyping) {
-			stopTimer();
-			console.log("end of sentence ${timer}");
-		}
 			}
+			if (!doneTyping) {
+				stopTimer();
+				console.log('end of sentence ${timer}');
+			}
+		}
 	}
 
-	 // Function to load typing problems from files
-  const loadTypingProblems = async () => {
-    try {
+	// Function to load typing problems from files
+	const loadTypingProblems = async () => {
+		try {
 			const i = Math.floor(Math.random() * 3) + 1;
 			console.log(i);
-      sentence = await fetch(`./problems/${i}.txt`).then(res => res.text());
-    } catch (error) {
-      console.error("Error loading typing problems:", error);
-    }
-  }
+			sentence = await fetch(`./problems/${i}.txt`).then((res) => res.text());
+		} catch (error) {
+			console.error('Error loading typing problems:', error);
+		}
+	};
 
 	const startTimer = () => {
-    startTime = performance.now();
-    timer = requestAnimationFrame(updateTimer);
-  };
+		startTime = performance.now();
+		timer = requestAnimationFrame(updateTimer);
+	};
 
-  const updateTimer = (currentTime) => {
+	const updateTimer = (currentTime) => {
 		if (!doneTyping) return;
-    elapsedSeconds = (currentTime - startTime) / 1000;
-    timer = requestAnimationFrame(updateTimer);
-  };
+		elapsedSeconds = (currentTime - startTime) / 1000;
+		timer = requestAnimationFrame(updateTimer);
+	};
 
-  const stopTimer = () => {
-    cancelAnimationFrame(timer);
-  };
+	const stopTimer = () => {
+		cancelAnimationFrame(timer);
+	};
 
-  // Load typing problems when component mounts
-  onMount(() => {
-    loadTypingProblems();
-  });
+	// Load typing problems when component mounts
+	onMount(() => {
+		loadTypingProblems();
+	});
 </script>
 
 <div>
 	<p>
 		{#each sentence as letter, index (index)}
-		<span class="character" style="background: {colors[typed[index]]}">{letter === ' ' ? '\u00A0' : letter}</span>
+			{#if !typed[index]}
+				<span id={'char' + index} class="character" style="background: {colors[typed[index]]}"
+					>{letter === ' ' ? '\u00A0' : letter}</span
+				>
+			{:else}
+				<span
+					in:fly={{ y: 100, duration: 500 }}
+					id={'char' + index}
+					class="character"
+					style=" background: {colors[typed[index]]}">{letter === ' ' ? '\u00A0' : letter}</span
+				>
+			{/if}
 		{/each}
 	</p>
 	<h1>Score: {score}</h1>
@@ -88,11 +100,11 @@
 	{/if}
 </div>
 
+<svelte:window on:keydown|preventDefault={onKeyDown} />
+
 <style>
 	.character {
 		display: inline-block;
 		padding: 0px;
 	}
 </style>
-
-<svelte:window on:keydown|preventDefault={onKeyDown} />
